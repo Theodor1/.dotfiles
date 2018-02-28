c53a9d41ff3e91a412c43fa46af68b6e50e134ea
Theo Silberbauer <33759304+Theodor1@users.noreply.github.com>
2018-02-28 21:20:20 +0100

    Update download.sh

--- download.sh
+++ download.sh
@@ -6,20 +6,20 @@
 
 #create new repository
 git init		
-echo "git init (ceate new repository)"
-echo "done..."
+echo "***echo: git init (ceate new repository)"
+echo "***echo: done..."
 
 #connect local repository to remote repository
 git remote add origin https://github.com/Theodor1/.dotfiles.git		
-echo "done..."
-echo "git remote add origin https://github.com/Theodor1/.dotfiles.git"
-echo "(connect local repository to remote repository)"
-echo "done..."
+echo "***echo: done..."
+echo "***echo: git remote add origin https://github.com/Theodor1/.dotfiles.git"
+echo "***echo: (connect local repository to remote repository)"
+echo "***echo: done..."
 
 #update local repository with remote chanes
 git pull origin master
-echo "git pull origin master"
-echo "done..."
+echo "***echo: git pull origin master"
+echo "***echo: done..."
 
 
 
@@ -40,20 +40,20 @@ files=`ls -1 $dir/__*`    # list of files/folders to symlink in homedir
 ##########
 
 # create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
+echo "***echo: Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
-echo "...done"
+echo "***echo: ...done"
 
 # change to the dotfiles directory
-echo "Changing to the $dir directory"
+echo "***echo: Changing to the $dir directory"
 cd $dir
-echo "...done"
+echo "***echo: ...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
     basename=`basename $file`
     targetname=`echo $basename | sed 's/__//'`
-    echo "Moving any existing dotfiles from ~ to $olddir"
+    echo "***echo: Moving any existing dotfiles from ~ to $olddir"
     mv ~/.$targetname $olddir/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$basename ~/.$targetname
@@ -83,6 +83,9 @@ files=`ls -1 $dir/__*`    # list of files/folders to symlink in homedir
 for file in $files; do
     basename=`basename $file`
     targetname=`echo $basename | sed 's/__//'`
-    echo "Creating symlink to $file in home directory."
+    echo "***echo: Creating symlink to $file in home directory."
     ln -s $dir/$basename ~/.$targetname
 done
+
+source ~/.bashrc
+source ~/.vimrc
